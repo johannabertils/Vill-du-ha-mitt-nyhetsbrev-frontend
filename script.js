768e18c3104e8102398c7fa2cbb46ca2d1ab2d36
Johanna Bertils <johannabertils@hotmail.com>
2021-04-21 19:08:45 +0200

    conneting login och register to database

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
+
+// fetch('http://localhost:3010/users', {
+//     credentials: 'same-origin'
+// });
+
 let root = document.getElementById("root");
 
 // Startpage
 let headerTemplate = `<div class="header"><h1> Välkommen! </h1></div>`;
-let loginBtn = `Har du ett konto? <button id="loginField"> Logga in här</button>`;
-let register = `Om inte <button id="registerField" > Registera dig här!</button>`;
+let login = `Har du ett konto? Logga in här `;
+let register = `Om inte, Registera dig här! `;
 
 let inputField = `<div id="logIn" class="logIn"><input type="text" placeholder="Användarnamn" id="userName">
-                  <input type="password" placeholder="Lösenord" id="passWord"> <button id="btn">Logga in</button><div class="btnArea"></div></div></div>`;
+                  <input type="password" placeholder="Lösenord" id="password"> <button id="loginBtn">Logga in</button></div></div></div>`;
 let createField = `<div><h2>Skapa konto</h2>
-<form action="/books/add" method="post">
-<div><input type="text" name="UserName"> Användarnamn </div> 
-<div><input type="password" name="password"> Lösenord </div> 
-<div><input type="email" name="email">Epostadress</div>
-<div><input type="checkbox" name="subscribe">Ja, jag vill få nyhetsbrev</div>
-<div><button type="submit">spara</button></div></form></div>`
+<div><input type="text" id="newUserName"> Användarnamn </div> 
+<div><input type="password" id="newPassword"> Lösenord </div> 
+<div><input type="email" id="email">Epostadress</div>
+<div><input type="checkbox" id="subscribe">Ja, jag vill få nyhetsbrev</div>
+<div><button id="submitBtn" type="button">spara</button></div></div>`
 
+
+root.insertAdjacentHTML("afterbegin", createField);
 root.insertAdjacentHTML("afterbegin", register);
-root.insertAdjacentHTML("afterbegin", loginBtn);
+root.insertAdjacentHTML("afterbegin", inputField);
+root.insertAdjacentHTML("afterbegin", login);
 root.insertAdjacentHTML("afterbegin", headerTemplate);
 
-// show loginfield
-loginField.addEventListener("click", function showLogin() {
-    root.insertAdjacentHTML("afterend", inputField);
+submitBtn.addEventListener("click", function () {
+    console.log("click");
+    let newUserInfo = {
+        userName: document.getElementById("newUserName").value,
+        password: document.getElementById("newPassword").value,
+        email: document.getElementById("email").value,
+        subscribe: document.getElementById("subscribe").checked
+    };
+    console.log(newUserInfo);
+
+    fetch("http://localhost:3010/users/new", {
+        method: "post",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            newUserInfo
+        })
+    }).then((res) => res.json())
+
 });
 
-// show registerfield
-registerField.addEventListener("click", function showRegister() {
-    root.insertAdjacentHTML("afterend", createField);
- 
-});
 
+loginBtn.addEventListener("click", function () {
+    let userInfo = {
+        userName: document.getElementById("userName").value,
+        password: document.getElementById("password").value
+    }
+
+    console.log("click");
+    console.log(userInfo);
+
+    fetch("http://localhost:3010/users/login", {
+        method: "post",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            userInfo
+        })
+    }).then((res) => res.json())
+    .then((json) => console.log(json));
+});
