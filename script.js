42851aa5da9b56cb2141b243d3e121db0e9c5137
Johanna Bertils <johannabertils@hotmail.com>
2021-04-21 22:53:17 +0200

    adding loggedinpage

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 
-// fetch('http://localhost:3010/users', {
-//     credentials: 'same-origin'
-// });
 
 let root = document.getElementById("root");
 
 // Startpage
-let headerTemplate = `<div class="header"><h1> Välkommen! </h1></div>`;
-let login = `Har du ett konto? Logga in här `;
-let register = `Om inte, Registera dig här! `;
+function startPage() {
+let headerTemplate = `<div id="headerTemplate" class="header"><h1> Välkommen! </h1></div>`;
+let login = `<div id="loginText">Har du ett konto? Logga in här </div>`;
+let register = `<div id="registerText">Om inte, Registera dig här! </div>`;
 
 let inputField = `<div id="logIn" class="logIn"><input type="text" placeholder="Användarnamn" id="userName">
                   <input type="password" placeholder="Lösenord" id="password"> <button id="loginBtn">Logga in</button></div></div></div>`;
-let createField = `<div><h2>Skapa konto</h2>
+let createField = `<div id="createField"><h2>Skapa konto</h2>
 <div><input type="text" id="newUserName"> Användarnamn </div> 
 <div><input type="password" id="newPassword"> Lösenord </div> 
 <div><input type="email" id="email">Epostadress</div>
 <div><input type="checkbox" id="subscribe">Ja, jag vill få nyhetsbrev</div>
-<div><button id="submitBtn" type="button">spara</button></div></div>`
+<div><button id="submitBtn" type="button">Skicka</button></div></div>`
 
 
 root.insertAdjacentHTML("afterbegin", createField);
@@ -48,7 +46,6 @@ submitBtn.addEventListener("click", function () {
 
 });
 
-
 loginBtn.addEventListener("click", function () {
     let userInfo = {
         userName: document.getElementById("userName").value,
@@ -67,5 +64,45 @@ loginBtn.addEventListener("click", function () {
             userInfo
         })
     }).then((res) => res.json())
-    .then((json) => console.log(json));
+    .then(function (data) {
+        console.log('Request succeeded with JSON response', data);
+        if (data === "Inloggad"){
+            console.log("yes");
+            loggedInPage(userName.value);
+        }
+        if (data === "fel inlogg"){
+            console.log("Fel användarnamn eller lösenord");
+            root.insertAdjacentHTML(`afterend`, `<div id="errorMessage"><p> Fel användarnamn eller lösenord! Försök igen.</p></div>`);
+        }
+      })
+    // .then((json) => 
+    // document.write(json == "inloggad"));
+    // console.log(json));
 });
+}
+
+function loggedInPage(userName) {
+    document.getElementById("createField").hidden=true;
+    document.getElementById("logIn").hidden=true;
+    document.getElementById("headerTemplate").hidden=true;
+    document.getElementById("loginText").hidden=true;
+    document.getElementById("registerText").hidden=true;
+    const name = userName;
+    root.insertAdjacentHTML("afterend", "<p> Välkommen, Du är nu inloggad som " + name + "!</p>");
+    let logOutBtn = `<div><button id="logOutBtn" type="button">Logga ut</button></div></div>`
+    root.insertAdjacentHTML("afterbegin", logOutBtn);
+    localStorage.setItem('loggedin', "true");
+    localStorage.setItem('loggedinuser', name);
+};
+
+if (localStorage.getItem("loggedin") === "true") {
+    loggedInPage(localStorage.getItem('loggedinuser'));
+};
+
+//RUN
+if (("loggedin") === "true") {
+    loggedInPage();
+}
+else {
+    startPage()
+}
